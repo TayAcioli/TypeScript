dbc055664a9a88bdf83e5300a5fe4d27750bde5f
Tayguara Acioli <tayguaracioli@gmail.com>
2018-08-30 21:27:32 -0700

    Add files via upload
    New model to encapsulate an array of Negociacao;
    Array and generics;
    DOM trough template;
    Dinamic Template;

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,14 +1,18 @@
 class NegociacaoController{
 
-    private _inputData      : HTMLInputElement;
-    private _inputQuantidade: HTMLInputElement;
-    private _inputValor     : HTMLInputElement;
+    private _inputData       : HTMLInputElement;
+    private _inputQuantidade : HTMLInputElement;
+    private _inputValor      : HTMLInputElement;
+    private _negociacoes     = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor(){
 
         this._inputData         = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade   = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor        = <HTMLInputElement>document.querySelector('#valor');
+        
+        this._negociacoesView.update(this._negociacoes);
 
     }
 
@@ -22,7 +26,8 @@ class NegociacaoController{
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao.volume);
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
 
     }
 
